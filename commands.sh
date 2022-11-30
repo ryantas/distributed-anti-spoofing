bf27c798425c7ff2cdc6c633e8c742ff64b24c9b
Richard Yantas <richard@dysopsis.com>
2022-11-30 15:55:40 -0500

    setup

---
+++ commands.sh
@@ -0,0 +1,16 @@
+sudo service redis stop
+sudo service redis status
+
+# ray start --head --redis-port=8888
+ray start --head
+
+# from cli on node
+# ray start --address='192.168.0.39:8888' --redis-password='5241590000000000'
+
+# from python on node
+# import ray 
+# ray.init(address='auto', redis-password='5241590000000000')
+
+# connect to different computer
+
+
